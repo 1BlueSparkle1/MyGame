bc9672e63787904e2db084f2f7d7526253d69631
Лера <kukuruzum@mail.ru>
2023-12-27 02:14:46 +0300

    Frames are differentiated by type, the main menu and initial settings are created, the exid button works, you can use the frame system to switch to settings and back to the menu

---
+++ MyGame/MyGame/MyPages/StartSetingsPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using MyGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1.MyPages
+{
+    /// <summary>
+    /// Логика взаимодействия для StartSetingsPage.xaml
+    /// </summary>
+    public partial class StartSetingsPage : Page
+    {
+        public static MainWindow mainWindow;
+        public StartSetingsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BackMenu_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.SettingFrameCenter.Visibility= Visibility.Collapsed;
+            mainWindow.AllWindowsFrame.Visibility= Visibility.Visible;
+            mainWindow.AllWindowsFrame.Navigate(new StartWindowPage());
+        }
+    }
+}
---
+++ MyGame/MyGame/MyPages/StartWindowPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using MyGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1.MyPages
+{
+    /// <summary>
+    /// Логика взаимодействия для StartWindowPage.xaml
+    /// </summary>
+    public partial class StartWindowPage : Page
+    {
+        public static MainWindow mainWindow;
+        public StartWindowPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Output_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void StartSetings_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.AllWindowsFrame.Visibility = Visibility.Collapsed;
+            mainWindow.SettingFrameCenter.Visibility = Visibility.Visible;
+            mainWindow.SettingFrameCenter.Navigate(new StartSetingsPage());
+        }
+    }
+}
